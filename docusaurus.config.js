f753a23c9f86ec5dea81ab39df1aa9bda539768d
Ali Eren Ekinci <alierenekinci@outlook.com>
2024-01-05 14:46:53 +0300

    sitemap added and docs edited

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,6 +76,17 @@ const config = {
         },
       }),
     ],
+    [
+      '@docusaurus/preset-classic',
+      {
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+        },
+      },
+    ],
   ],
 
   themeConfig:
